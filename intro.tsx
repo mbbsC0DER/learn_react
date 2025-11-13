343991f9553cbb2771f39702f58b801b57526a0c
mbbsC0DER <kprathamnegi05@gmail.com>
2025-11-14 00:56:56 +0530

    the first commit of this react repo

---
+++ intro.tsx
@@ -0,0 +1,6 @@
+//why React ? 
+//make easy to manage & build complex frontend .
+//React is a library (cool dude - strict rules nhi hote)
+//SPA = Single Page Application (page not reload)
+// Cons = no  SEO ,  browser render of js slow , no routing 
+
